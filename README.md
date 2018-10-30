be1e379a0af38c77d82c80d1082aeb12f9a7039d
Justin <justinbkay@gmail.com>
2018-10-29 19:01:10 -0600

    Convert to a class and add readme

---
+++ README.md
@@ -0,0 +1,12 @@
+Bundle install
+
+Add site urls to sites.txt, one url per line:
+
+https://www.google.com
+https://www.nike.com
+
+
+Then run the ruby script with an argument of the path to the sites you want to download:
+
+ruby ./download_files.rb sites.txt
+
--- download_files.rb
+++ download_files.rb
@@ -2,14 +2,22 @@ require 'rubygems'
 require 'wayback_machine_downloader'
 gem 'wayback_machine_downloader', '~> 2.2.1'
 
-@path = `bundle show wayback_machine_downloader --paths`.chomp
-@path += '/bin/wayback_machine_downloader'
+class DownloadFiles
+  def initialize(path = './sites.txt')
+    @file = path
+    @path = `bundle show wayback_machine_downloader --paths`.chomp
+    @path += '/bin/wayback_machine_downloader'
+  end
 
-file = ARGV[0]
+  def download_sites
+    contents = File.read(@file)
 
-contents = File.read(file)
-
-contents.each_line do |url|
-  puts "Dowloading files for #{url}"
-  `ruby #{@path} #{url}`
+    contents.each_line do |url|
+      puts "Dowloading files for #{url}"
+      `ruby #{@path} #{url}`
+    end
+  end
 end
+
+a = DownloadFiles.new(ARGV[0])
+a.download_sites
